49706feeacede1d369407cd39364162f682444e0
EvgenyMinin <evgeny.frontend@gmail.com>
2021-03-16 19:40:22 +0300

    added custom fonts

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 import MealDetailsScreen from './screens/MealDetailsScreen';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontLoaded) {
+    return <AppLoading />;
+  }
   return (
     <View style={styles.container}>
       <MealDetailsScreen />
