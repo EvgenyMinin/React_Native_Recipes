09e4667db7bde366d3a10b465ca4b84e5de0d843
EvgenyMinin <evgeny.frontend@gmail.com>
2021-03-20 22:07:06 +0300

    added options to header

--- App.jsx
+++ App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import CategoryMealsScreen from './screens/CategoryMealsScreen';
 import MealDetailsScreen from './screens/MealDetailsScreen';
+import Colors from './constants/Colors';
+import { Platform } from 'react-native';
 
 const MealsStack = createStackNavigator();
 
@@ -22,8 +24,23 @@ export default function App() {
   return (
     <NavigationContainer>
       <MealsStack.Navigator>
-        <MealsStack.Screen name="Meal Categories" component={CategoriesScreen} />
-        <MealsStack.Screen name="Meal Category" component={CategoryMealsScreen} />
+        <MealsStack.Screen
+          name="Meal Categories"
+          component={CategoriesScreen}
+          options={{
+            title: 'Overview',
+            headerStyle: {
+              backgroundColor:
+                Platform.OS === 'android' ? Colors.primary : 'white',
+            },
+            headerTintColor:
+              Platform.OS === 'android' ? 'white' : Colors.primary,
+          }}
+        />
+        <MealsStack.Screen
+          name="Meal Category"
+          component={CategoryMealsScreen}
+        />
         <MealsStack.Screen name="Meal Detail" component={MealDetailsScreen} />
       </MealsStack.Navigator>
     </NavigationContainer>
