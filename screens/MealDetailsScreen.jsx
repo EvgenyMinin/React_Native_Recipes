da9671acd0a70a2bd901c2d0e7abb48582d09c1d
EvgenyMinin <evgeny.frontend@gmail.com>
2021-03-29 20:29:01 +0300

    added detail meal screen

--- screens/MealDetailsScreen.jsx
+++ screens/MealDetailsScreen.jsx
@@ -1,18 +1,63 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import Center from '../components/Center';
+import DefaultText from '../components/DefaultText';
 import { MEALS } from '../data/dummyData';
+
+const ListItem = ({ children }) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailsScreen = ({ route }) => {
   const { mealId } = route.params;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <Center>
-      <Text>{selectedMeal.title}</Text>
-    </Center>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  details: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 12,
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default MealDetailsScreen;
